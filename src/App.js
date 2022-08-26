28524149170f1a4e23c6f5e715b69b7687e28f0e
MỸ NGÂN <myngan@MacBook-Air-cua-MY-NGAN.local>
2022-08-26 21:32:46 +0700

    blablabla

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [quests, setQuests] = useState(() => {
+    const storageQuests = JSON.parse(localStorage.getItem('jobs'))
+    return storageQuests ?? []
+  })
+  const [quest, setQuest] = useState('')
+  const [index, setIndex] = useState('')
+  
+  const handleSubmit = () => {
+    setQuests(prev => {
+      const newQuests = [...prev, quest]
+
+      // save to localStorage
+      const jsonQuests = JSON.stringify(newQuests)
+      localStorage.setItem('jobs', jsonQuests)
+
+      return newQuests
+    })
+    setQuest('')
+  }
+
+  const handleDelete = () => {
+    quests.shift()
+    // console.log(quests)
+    setQuests(prev => {
+      const newQuests = [...prev, index]
+
+      // save to localStorage
+      const jsonQuests = JSON.stringify(newQuests)
+      localStorage.setItem('jobs', jsonQuests)
+
+      return newQuests
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: 32 }}>
+      <h2>todos</h2>
+      <input 
+        value={quest}
+        onChange={e => setQuest(e.target.value)}
+      />
+      <button onClick={handleSubmit}>Add</button>
+
+      <ul>
+        {quests.map((quest, index) => (
+          <div key={index}>
+            <li>{quest}</li>
+            <button onClick={handleDelete}>Cút</button>
+          </div>
+        ))}
+      </ul>
     </div>
   );
 }
