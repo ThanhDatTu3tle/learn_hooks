0f8223ead5a4777264236f3fa83fd2c12aaa9151
ThanhDatTu3tle <thanhdat11badinh@gmail.com>
2022-08-30 02:04:51 +0700

    update useEffect

--- src/App.js
+++ src/App.js
@@ -1,55 +1,19 @@
 // import logo from './logo.svg';
 // import './App.css';
+// import * as React from 'react';
+// import Content from './component/Content';
+// import Button from '@mui/material/Button';
+// import Input from '@mui/material/Input';
 import { useState } from "react";
+import Content from './component/Content';
 
 function App() {
-  const [task, setTask] = useState('')
-  const [tasks, setTasks] = useState(() => {
-    const storageTasks = JSON.parse(localStorage.getItem('Tasks'))
-
-    return storageTasks ?? []
-  })
-
-  const handleSubmit = () => {
-    if (!task) return;
-    setTasks(prev => {
-      const newTasks = [...prev, task]
-
-      const jsonTasks = JSON.stringify(newTasks)
-      localStorage.setItem('Tasks', jsonTasks)
-
-      return newTasks
-    })
-    setTask('')
-  }
-
-  const handleDelete = (curTask) => {
-    const tasksList = tasks.filter((task) => (task !== curTask))
-    setTasks(tasksList)
-    localStorage.setItem('Tasks', tasksList)
-  }
+  const [show, setShow] = useState(false)
 
   return (
     <div className="App" style={{ padding: 32 }}>
-      <h1>todos</h1>
-      <input 
-        value={task} 
-        onChange={e => setTask(e.target.value)}
-      />
-      <button onClick={handleSubmit}>
-        Add
-      </button>
-
-      <ul>
-        {tasks.map((task, index) => (
-          <li key={index}>
-            {task}
-            <button onClick={() => handleDelete(task)}>
-              Cút
-            </button>
-          </li>
-        ))}
-      </ul>
+      <button onClick={() => setShow(!show)}>Toggle</button>
+      {show && <Content/>}
     </div>
   );
 }
---
+++ src/component/Content.js
@@ -0,0 +1,90 @@
+import { useState, useEffect } from "react";
+
+// 1. useEffect(callback)
+// - Gọi callback mỗi khi component re-render
+// - Gọi callback sau khi component thêm element vào DOM
+
+// 2. useEffect(callback, [])
+// - Chỉ gọi callback một lần sau khi component mounted
+
+// 3. useEffect(callback, [deps])
+// - Callback sẽ được gọi lại mỗi khi deps thay đổi
+
+// ----------------------------------
+// Lý thuyết chung
+// 1. Callback luôn được gọi sau khi component mounted
+// 2. Cleanup function luôn được gọi trước khi component unmounted
+
+const tabs = ['posts', 'comments', 'albums', 'photos', 'todos', 'users']
+
+function Content() {
+
+  const [title, setTitle] = useState('')
+  const [types, setTypes] = useState('posts')
+  const [contents, setContents] = useState([])
+  const [showGoToTop, setShowGoToTop] = useState(false)
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${types}`)
+      .then(res => res.json())
+      .then(contents => {
+        setContents(contents)
+      })
+  }, [types])
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 200) {
+        setShowGoToTop(true)
+      } else {
+        setShowGoToTop(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    // cleanup function
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  return (
+    <div>
+      {tabs.map(tab => (
+        <button 
+          key={tab}
+          onClick={() => setTypes(tab)}
+          style={types === tab ? { 
+            color: 'white', 
+            backgroundColor: 'gray' } : {}}
+        >
+          {tab}
+        </button>
+      ))}
+      <input 
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+      />
+      <ul>
+        {contents.map(content => (
+          <li key={content.id}>
+            {content.title || content.name}
+          </li>
+        ))}
+      </ul>
+      {showGoToTop && (
+        <button
+        style={{
+          position: 'fixed',
+          right: 20,
+          bottom: 20
+        }}>
+          Go to top
+        </button>
+      )}
+    </div>
+  )
+}
+
+export default Content;
